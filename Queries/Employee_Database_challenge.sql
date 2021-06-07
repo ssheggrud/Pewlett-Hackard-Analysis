820c090646fd97115bf5e62b15c6b1321383a756
Sherry Sheggrud <sheggrud@gmail.com>
2021-06-06 23:01:25 -0400

    uploading title csv files

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,1 +1,32 @@
+-- creating Retirement Titles table
+SELECT e.emp_no,
+    e.first_name,
+	e.last_name,
+    ti.title,
+	ti.from_date,
+	ti.to_date
+INTO retirement_titles
+FROM employees as e
+LEFT JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no, title;
 
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+
+INTO unique_titles
+FROM retirement_titles as rt
+ORDER BY emp_no ASC, title DESC;
+
+-- number of employees by their most recent job title about to retire
+SELECT COUNT(ut.emp_no),
+ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY title 
+ORDER BY COUNT(title) DESC;
