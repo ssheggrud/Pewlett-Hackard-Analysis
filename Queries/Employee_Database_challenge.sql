081726c8fc4fb06dc2b5e8930594051c4a2d0b5d
Sherry Sheggrud <sheggrud@gmail.com>
2021-06-06 23:31:59 -0400

    adding final version of all challenge csv files and sql

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,3 +1,4 @@
+-- Deliverable 1: The Number of Retiring Employees by Title
 -- creating Retirement Titles table
 SELECT e.emp_no,
     e.first_name,
@@ -10,8 +11,7 @@ FROM employees as e
 LEFT JOIN titles as ti
 ON (e.emp_no = ti.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-ORDER BY e.emp_no, title;
-
+ORDER BY e.emp_no ASC, to_date DESC;
 
 -- Use Dictinct with Orderby to remove duplicate rows
 SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
@@ -21,7 +21,7 @@ rt.title
 
 INTO unique_titles
 FROM retirement_titles as rt
-ORDER BY emp_no ASC, title DESC;
+ORDER BY emp_no ASC, to_date DESC;
 
 -- number of employees by their most recent job title about to retire
 SELECT COUNT(ut.emp_no),
@@ -30,3 +30,22 @@ INTO retiring_titles
 FROM unique_titles as ut
 GROUP BY title 
 ORDER BY COUNT(title) DESC;
+
+-- Deliverable 2: The Employees Eligible for the Mentorship Program
+-- creating Mentorship Eligibility Table
+SELECT DISTINCT ON(e.emp_no) e.emp_no, 
+    e.first_name, 
+    e.last_name, 
+    e.birth_date,
+    de.from_date,
+    de.to_date,
+    ti.title
+INTO mentorship_eligibilty
+FROM employees as e
+LEFT OUTER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+LEFT OUTER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no ASC;
